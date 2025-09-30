9d7e3f1d6697eb8578cff0ff6fa0f6651dcf6703
anankaji <163483034+anankajii@users.noreply.github.com>
2025-09-30 14:45:21 +0700

    Add files via upload

---
+++ person.php
@@ -0,0 +1,12 @@
+<?php
+    class person {
+        var $name;
+        function set_name($name) {
+            $this->name = $name;
+        }
+
+        function get_name() {
+            return $this->name;
+        }
+    }
+?>
