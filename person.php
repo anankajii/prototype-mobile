70bc9abc0cf7c91fec7f336223721112e85a198d
anankaji <163483034+anankajii@users.noreply.github.com>
2025-09-30 14:46:57 +0700

    Update person.php

--- person.php
+++ person.php
@@ -1,12 +1,1 @@
-<?php
-    class person {
-        var $name;
-        function set_name($name) {
-            $this->name = $name;
-        }
 
-        function get_name() {
-            return $this->name;
-        }
-    }
-?>
